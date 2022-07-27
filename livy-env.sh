a8d88815202af5383dfea65e468ddc274c8bedf5
Pravin Bhagade <pbhagade@cloudera.com>
2022-07-27 10:44:54 +0530

    Update livy-env.sh

--- livy-env.sh
+++ livy-env.sh
@@ -1,5 +1,5 @@
 
-            #!/usr/bin/env bash
+#!/usr/bin/env bash
 
             # - SPARK_HOME      Spark which you would like to use in livy
             # - SPARK_CONF_DIR  Directory containing the Spark configuration to use.
@@ -7,10 +7,10 @@
             # - LIVY_LOG_DIR    Where log files are stored.  (Default: ${LIVY_HOME}/logs)
             # - LIVY_PID_DIR    Where the pid file is stored. (Default: /tmp)
             # - LIVY_SERVER_JAVA_OPTS  Java Opts for running livy server (You can set jvm related setting here, like jvm memory/gc algorithm and etc.)
-            export SPARK_HOME=/usr/hdp/current/spark2-client
-            export SPARK_CONF_DIR=/etc/spark2/conf
-            export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk
-            export HADOOP_CONF_DIR=/etc/hadoop/conf
-            export LIVY_LOG_DIR=/var/log/livy2
-            export LIVY_PID_DIR=/var/run/livy2
-            export LIVY_SERVER_JAVA_OPTS="-Xmx2g"
+export SPARK_HOME=/usr/odp/current/spark2-client
+export SPARK_CONF_DIR=/etc/spark2/conf
+export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-1.8.0.332.b09-1.el7_9.x86_64/jre
+export HADOOP_CONF_DIR=/etc/hadoop/conf
+export LIVY_LOG_DIR=/var/log/livy2
+export LIVY_PID_DIR=/var/run/livy2
+export LIVY_SERVER_JAVA_OPTS="-Xmx2g"
